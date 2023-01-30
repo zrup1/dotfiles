8b08c94e687c0490e59f18b8fa340e05e8c57f71
zrup1 <95546771+zrup1@users.noreply.github.com>
2023-01-30 17:40:49 +0200

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting
+alias vim="nvim"
+alias update="paru -Syyu"
+alias ro="setxkbmap -layout ro"
+alias us="setxkbmap -layout us"
+fish_add_path /home/vale/.spicetify
